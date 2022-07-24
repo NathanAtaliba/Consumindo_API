987387a0926a69ab90f2428d05a026812d82e24e
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2022-07-24 20:44:55 -0300

    consumindo_API

--- Consumindo_api/src/main/java/main.java
+++ Consumindo_api/src/main/java/main.java
@@ -10,16 +10,14 @@ public class main {
     public static void main(String[] args) throws  Exception {
 // fazer uma conexão HTTP e buscar os top 250 filmes
         String url = "https://api.themoviedb.org/3/trending/movie/week?api_key=872995efee79646f5b0d834c33522673"; //URL do arquivo JSON
-
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
-
+        URI endereco = URI.create(url);  // Salvando o URL em uma URI (URL é o endereço de um recurso na internet , URI é o identificador uniforme de recurso)
+        var client = HttpClient.newHttpClient(); // criando um novo httpClient
+        var request = HttpRequest.newBuilder(endereco).GET().build(); //Fazendo um request
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString()); //Implementações do BodyHandler que implementam vários manipuladores úteis, como lidar com o corpo de resposta como uma String ou transmitir o corpo de resposta para um arquivo.
+        String body = response.body(); //Criando uma String para salvar a resposta (body() é  um metodo da classe HttpResponse)
         // extrair só os dados que interessam (titulo, poster, classificação)
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        var parser = new JsonParser(); //O parser é um analisador sintático. Sua função é ler uma entrada de dados que possuem certas regras específicas (O arquivo foi separado por alguns regex)
+        List<Map<String, String>> listaDeFilmes = parser.parse(body); //Criando uma lista mapeada por uma chave em String e conteudo String para salvar cada Filme em um objeto
         // exibir e manipular os dados
         for (Map<String,String> filme : listaDeFilmes) {
             System.out.println(filme.get("title"));
